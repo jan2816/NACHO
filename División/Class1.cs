451b20e53e580f4f009acd7419411d9096fe7a55
Isabella0038 <142461650+Isabella0038@users.noreply.github.com>
2023-09-05 14:10:22 -0500

    Add files via upload

---
+++ División/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace División
+{
+    public class Class1
+    {
+        int i = 0;
+        int j = 0;
+
+        public static long División(long i, long j)
+        {
+            return i / j;
+        }
+        
+
+
+    }
+}

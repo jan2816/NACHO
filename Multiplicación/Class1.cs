41ada9293cd2245594673c03530d37d134102f48
Isabella0038 <142461650+Isabella0038@users.noreply.github.com>
2023-09-05 13:40:48 -0500

    Add files via upload

---
+++ Multiplicación/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Multiplicación
+{
+    public class Class1
+    {
+        int i = 0;
+        int j = 0;
+        public static long Multiplicación(long i, long j)
+        {
+            return i * j;
+        }
+
+    }
+}

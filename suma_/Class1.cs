58b21530b56744e2b898a53b66f0ecae886d90e0
Valenargaezceballos02 <142552161+Valenargaezceballos02@users.noreply.github.com>
2023-09-05 21:22:28 -0500

    Add files via upload

---
+++ suma_/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_
+{
+    public class Class1
+    {
+        int i = 0;
+        int j = 0;
+
+        public static long Suma (long i, long j)
+        {
+            return i + j;
+        }
+    }
+}

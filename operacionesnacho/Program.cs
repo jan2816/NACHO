0b21f6ca089aed7af8e0c2fd67334e855165f9c0
jan2816 <142441334+jan2816@users.noreply.github.com>
2023-09-06 18:14:53 -0500

    Add files via upload

---
+++ operacionesnacho/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using operaciones;
+
+namespace operacionesnacho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, i;
+            Console.Write("digita el primer numero; ");
+            j = int.Parse(Console.ReadLine());
+            Console.Write("digita el primer numero: ");
+            i = int.Parse(Console.ReadLine());
+
+            long sumar = operaciones.operaciones.suma(j, i);
+            long res = operaciones.operaciones.resta(j, i);
+            long mul = operaciones.operaciones.multiplicacion(j, i);
+            long divi = operaciones.operaciones.division(j, i);
+
+            Console.WriteLine("la suma es:"+sumar);
+            Console.WriteLine("la resta es:" + res);
+            Console.WriteLine("la multiplicacion es:" + mul);
+            Console.WriteLine("la division es:" + divi);
+            Console.ReadKey();
+        }
+    }
+}
